a65558bb8141a148deb84e3849edcb1c839f0bfe
Pranjal <pranjal10092005@gmail.com>
2026-05-01 09:16:31 +0530

    add frontend url to backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://team-task-manager-1-899j.onrender.com"
+    ],
     credentials: true,
 }));
 connectDB();
